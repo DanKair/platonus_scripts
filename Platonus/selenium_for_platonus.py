d72be1f4e45d381f8108b176ff499bd74b892f99
DanKair <kair.daniyaruyl@gmail.com>
2025-06-14 10:56:37 +0500

    Added Scroll feature, to fetch remaining grades;

--- Platonus/selenium_for_platonus.py
+++ Platonus/selenium_for_platonus.py
@@ -11,7 +11,7 @@ load_dotenv()
 user_login = os.getenv("LOGIN")
 user_password = os.getenv("PASSWORD")
 grade_url = os.getenv("GRADE_URL")
-screenshot_folder = os.getenv("TARGET_FOLDER")
+# screenshot_folder = os.getenv("TARGET_FOLDER")
 
 # Set up the WebDriver
 driver = webdriver.Chrome()
@@ -31,20 +31,44 @@ password_field.send_keys(user_password)
 login_button = driver.find_element(By.ID, "Submit1")
 login_button.click()
 
-time.sleep(3)
+time.sleep(2)
 
 # Go To Grade field
 
 driver.get(grade_url)
-time.sleep(3)
+time.sleep(2)
 
-# Capture screenshot and save it to a file
-# driver.save_screenshot("grades.png") # Saves screenshot to the current working directory
+
+os.getcwd() # If you don't know path, uncomment this
+if not os.path.exists("screenshots"):
+    os.mkdir("screenshots")
+os.chdir("screenshots")
+
+# Capture screenshot to "screenshot" folder and save it
+driver.save_screenshot("grades1.png")
+
+driver.execute_script("window.scrollBy(0, 600)") # Scroll down to 600 pixels
+time.sleep(1)
+driver.save_screenshot("grades2.png")
+
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(1)
+driver.save_screenshot("grades3.png")
+time.sleep(2)
 
 # Or You can save it to folder of your choice by changing to it:
-os.chdir(screenshot_folder)
-driver.save_screenshot("grades.png")
-time.sleep(3)
+# os.chdir(screenshot_folder)
+# driver.save_screenshot("grades1.png")
+#
+# driver.execute_script("window.scrollBy(0, 600)") # Scroll down to 600 pixels
+# time.sleep(1)
+# driver.save_screenshot("grades2.png")
+# time.sleep(2)
+#
+# driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+# time.sleep(1)
+# driver.save_screenshot("grades3.png")
+# time.sleep(2)
 
 # Close the browser
 driver.quit()
